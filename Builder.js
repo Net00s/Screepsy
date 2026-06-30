41aae2f4347d4be604d2fd3e4484f575ab4f3a28
Net00s <Neo07@jutstrom.se>
2026-07-01 00:59:45 +0200

    stuff

--- Builder.js
+++ Builder.js
@@ -1,28 +1,27 @@
-var roleUpgrader = {
+var roleBuilder = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
 
         var sources = creep.room.find(FIND_SOURCES)
-        var RoomController = creep.room.controller
-
+        var spawn = Game.spawns["Spawn1"]
+        let targetBuild = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES)
         let target = sources[creep.name.split("_")[1] % sources.length]
 
-
         if (creep.store.getUsedCapacity() == 0) {
-            creep.memory.upgrading = false;
+            creep.memory.building = false;
         }
 
         if (creep.store.getFreeCapacity() == 0) {
-            creep.memory.upgrading = true;
+            creep.memory.building = true;
         }
         
-        if (creep.memory.upgrading == true){
-            if (creep.upgradeController(RoomController) == ERR_NOT_IN_RANGE){
-                creep.moveTo(RoomController)
+        if (creep.memory.building == true){
+            if (creep.build(targetBuild) == ERR_NOT_IN_RANGE){
+                creep.moveTo(targetBuild)
             }
         }
-        else if (creep.memory.upgrading == false){
+        else if (creep.memory.building == false){
             if (creep.harvest(target) == ERR_NOT_IN_RANGE){
                 creep.moveTo(target)
             }
@@ -30,4 +29,4 @@ var roleUpgrader = {
     }
 }
 
-module.exports = roleUpgrader;
+module.exports = roleBuilder;
--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 var roleHarvester = require('harvester');
 var roleUpgrader = require('upgrader');
+var roleBuilder = require('Builder')
 var spawn = Game.spawns["Spawn1"]
 
 module.exports.loop = function () {
 
 var harvesters = Object.keys(Game.creeps).filter(name => name.includes("Harvester"))
 var upgraders = Object.keys(Game.creeps).filter(name => name.includes("Upgrader"))
-
+var builders = Object.keys(Game.creeps).filter(name => name.includes("Builder"))
 
 
     for(var name in Game.creeps) {
@@ -17,9 +18,21 @@ var upgraders = Object.keys(Game.creeps).filter(name => name.includes("Upgrader"
         if(creep.name.includes("Upgrader")) {
             roleUpgrader.run(creep);
         }
+        if(creep.name.includes("Builder")) {
+            roleBuilder.run(creep);
+        }
     }
 
     //when multible spawnCreep functions are run in the same tick, the last one is used. Harvesters have higher priority than upgraders
+
+    if(spawn.room.energyAvailable == spawn.room.energyCapacityAvailable){
+        createCreep([WORK, CARRY, CARRY, MOVE, MOVE], `SUPERHarvester_${harvesters.length + 666}`)
+    }
+
+    if(builders.length < 2){
+        createCreep([WORK, CARRY, MOVE], `Builder_${builders.length}`)
+    }
+
     if(upgraders.length < 1){
         createCreep([WORK, CARRY, MOVE], `Upgrader_${upgraders.length}`)
     }
